c60348cb075975368862bc4bbde699840fdbe062
Moritz Laass <moritz.laass@gmail.com>
2011-04-30 18:16:15 +0200

    jumping block

--- main.js
+++ main.js
@@ -1,5 +1,10 @@
 
-define(['jo/src/jo', 'jo/src/Game', 'jo/src/TileMap'], function(jo, Game, Map){
+define(['./jo/src/jo', 
+        './jo/src/Game', 
+        './jo/src/TileMap',
+        './jo/src/math2d',
+        './objects/Actor'], 
+		function(jo, Game, Map, m2d, Actor){
 	
 	//helps with the debugging in firebug
 	$jo = jo;
@@ -23,22 +28,31 @@ define(['jo/src/jo', 'jo/src/Game', 'jo/src/TileMap'], function(jo, Game, Map){
 	
 	game.ready(function(){
 		start();
-		game.ts = new jo.TileSet([1,2,3,4], 64,64, jo.files.img.tileset);
+		game.ts = new jo.TileSet([0,1,2,3,], 64,64, jo.files.img.tileset);
 		game.map = new Map(game.ts, 32, 26, null);
-		for(var i=0; i< 32; i++){
+		for(var i=0; i< 10; i++){
 			game.map.put(i,5,{index: i%4});
 		}
+		game.map.put(10,4,{index: i%4});
+		game.map.put(11,4,{index: i%4});
+		game.map.put(12,4,{index: i%4});
+		game.add(new Actor({name: 'player', position: new jo.Point(100, 64)}), 'player');
 		
 	});
 	game.OnUpdate(function(ticks){
+		var player = this.get('player');
 		
-		if(game.state ===  'play'){
-			if(jo.input.key(jo.input.UP)){
+			if(jo.input.once('SPACE')){
+				player.jump();
 			}
-			if(jo.input.key(jo.input.DOWN)){
-
+			if(jo.input.k('RIGHT')){
+				player.side(1);
+			}else if(jo.input.k('LEFT')){
+				player.side(-1);
+			}else{
+				player.stand();
 			}
-		}
+		//player.pos.copy(game.cam.toWorld(jo.input.mouse));
 		if(jo.input.key[jo.input.D] ){
 			jo.log(game.map.data);
 		}
@@ -46,16 +60,39 @@ define(['jo/src/jo', 'jo/src/Game', 'jo/src/TileMap'], function(jo, Game, Map){
 
 			game.cam.subtract(jo.input.mouseMovement());
 		}
-
+		game.handleCollision();
 	});
 	var caption = function(msg){
-		jo.screen.text({align: 'center', fill: jo.clr.white, stroke: 0}, new jo.Point(jo.screen.width/2, jo.screen.height/2), msg);
+		jo.screen.text({ fill: jo.clr.white, stroke: 0}, new jo.Point(64, 32), msg);
 	};
 	game.OnDraw(function() {
 		jo.screen.clear(jo.color(0,0,0));
 		game.map.draw({}, game.cam.toScreen(), jo.screen);
+		for(var i in game.tiles){
+			//jo.screen.rect({fill:game.tiles[i].hit,stroke: 'yellow'}, game.cam.toScreen(game.tiles[i].pos), 64,64);
+		}
 		caption('state: '+game.state);
-		jo.files.img.test.draw({angle: (jo.screen.frames/60)*Math.PI,pivot: 'center'}, new jo.Point(jo.screen.width/2, jo.screen.height/2-32), jo.screen);
+		jo.files.img.test.draw({angle: (jo.screen.frames/60)*Math.PI,pivot: 'center'}, new jo.Point(32, 32), jo.screen);
 	});
+	game.handleCollision = function(){
+		for(var i in this.objects){
+			game.mapCollide(game.map, this.objects[i]);
+		}
+	};
+	game.mapCollide = function(map, actor){
+		var tiles = game.tiles = map.getIntersection({x:actor.pos.x, y: actor.pos.y, width: actor.width, height: actor.height});
+		
+		for(var i in tiles){
+			jo.screen.rect({fill:'yellow',stroke: 'yellow'}, game.cam.toScreen(tiles[i].pos), 64,64);
+			if(tiles[i].index >= 0 && m2d.intersect.boxBox(tiles[i], actor)){
+				actor.ground = true;
+				tiles[i].hit="yellow";
+				actor.pos.y = tiles[i].pos.y - actor.width;
+			}else{
+				tiles[i].hit=0;
+				actor.ground=false;
+			}
+		}
+	};
 	
 });
---
+++ objects/Actor.js
@@ -0,0 +1,59 @@
+define(['../jo/src/jo', '../jo/src/Object'],function(jo, Object){
+	
+	jo.Actor = jo.Object.extend({
+		
+		joObject: 'Actor',
+		
+		init: function(options){
+			this._super(options);
+			this.name = options.name;
+			this.pos = options.position.clone();
+			this.lp= this.pos.clone();
+			this.width = this.height = 32;
+			this.ground = false;
+			this.landed= 200;
+		},
+		draw: function(srf){
+			this._super(srf);
+			var p = jo.game.cam.toScreen(this.pos);
+			srf.rect({fill:this.ground?'#00ff00':'#ff00ff', stroke: 'white'}, p,this.width, this.height);
+			srf.text({fill:'#999', align: 'left', baseline: 'bottom', font:'12px console', stroke: 0}, p, this.name);
+		},
+		update: function(ticks){
+			var pp = this.pos.clone();
+			
+			if(!this.ground){
+				this.pos.y+=1;
+				this.pos.y += this.pos.y-this.lp.y;
+				this.pos.x += this.pos.x-this.lp.x;
+			}else{
+				this.landed+=ticks;
+				this.pos.x += (this.pos.x-this.lp.x)*0.5;
+			}
+			
+			this.lp = pp;
+			
+			
+		},
+		jump: function(){
+			if(this.ground && this.landed > 25){
+				this.ground = false;
+				this.pos.y -= 14;
+				this.side(this.dir);
+				this.landed=0;
+			}
+		},
+		side: function(dir){
+			this.dir= dir;
+			if(this.ground){
+				this.pos.x+=4*dir;
+			}else{
+				this.pos.x+=1*dir;
+			}
+		},
+		stand: function(){
+			this.dir=0;
+		}
+	});
+	return jo.Actor;
+});
